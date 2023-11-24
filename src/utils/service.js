1b920ca65a8bd27851053f4c8b4050ab4063e484
SJYI <525746338@qq.com>
2023-11-24 16:02:36 +0800

    响应拦截器

--- src/utils/service.js
+++ src/utils/service.js
@@ -1,24 +1,39 @@
-import axios from 'axios'
+import axios from 'axios';
+import { Message } from 'element-ui';
 
 // 配置基本URL
 const service = axios.create({
-  baseURL: window.location.origin + '/api'
-})
-// 请求拦截器
-service.interceptors.request.use(config => {
-  // 可在此处对请求进行处理，例如添加请求头、设置认证信息等
-  return config;
-}, error => {
-  return Promise.reject(error);
+  baseURL: window.location.origin + '/api',
 });
+// 请求拦截器
+service.interceptors.request.use(
+  (config) => {
+    // 可在此处对请求进行处理，例如添加请求头、设置认证信息等
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
 
 // 响应拦截器
-service.interceptors.response.use(response => {
-  // 可在此处对响应进行处理，例如处理错误码、格式化数据等
-  return response.data;
-}, error => {
-  return Promise.reject(error);
-});
+service.interceptors.response.use(
+  (response) => {
+    // 可在此处对响应进行处理，例如处理错误码、格式化数据等
+    if (response.data.code == 0) {
+      return response.data;
+    } else {
+      Message.error(response.data.message);
+      if (response.data.code == 'not_login') {
+        this.$route.push('/');
+      }
+    }
+  },
+  (error) => {
+    Message.error('服务异常');
+    return Promise.reject(error);
+  },
+);
 
 const http = {
   get(url, params = {}) {
@@ -27,8 +42,8 @@ const http = {
       params,
       method: 'get',
       headers: {
-        "satoken": localStorage.getItem("satoken")
-      }
+        satoken: localStorage.getItem('satoken'),
+      },
     });
   },
   post(url, data = {}) {
@@ -37,8 +52,8 @@ const http = {
       data,
       method: 'post',
       headers: {
-        "satoken": localStorage.getItem("satoken")
-      }
+        satoken: localStorage.getItem('satoken'),
+      },
     });
   },
   delete(url, params = {}) {
@@ -54,8 +69,8 @@ const http = {
       data,
       method: 'put',
       headers: {
-        "satoken": localStorage.getItem("satoken")
-      }
+        satoken: localStorage.getItem('satoken'),
+      },
     });
   },
   formDate(url, data = {}) {
@@ -65,10 +80,10 @@ const http = {
       data,
       headers: {
         'Content-Type': 'multipart/form-data',
-        "satoken": localStorage.getItem("satoken")
-      }
-    })
-  }
+        satoken: localStorage.getItem('satoken'),
+      },
+    });
+  },
 };
 
 export default http;
