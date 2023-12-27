540b5a1e658ac0c67cc4a33b9e4f606f65ad72f1
SJYI <525746338@qq.com>
2023-12-27 16:40:11 +0800

    用户管理-专业班级年级选择条件可单选、且只有在角色为空或学生时可选

--- src/api/user.js
+++ src/api/user.js
@@ -1,6 +1,14 @@
 import http from '@/utils/service';
 
 /**
+ * 用户登录
+ * @param account
+ * @param password
+ * @returns {*}
+ */
+export const loginService = ({ account, password }) => http.post('/user/login', { account, password });
+
+/**
  * 修改用户信息信息
  * @param {{ oldpwd, password, username, phone }} param0
  * @returns
@@ -14,4 +22,8 @@ export const saveUserInfoService = ({ oldpwd, password, username, phone }) =>
  */
 export const getUserInfoService = () => http.get('/user/getInfo');
 
+/**
+ * 用户退出
+ * @returns {*}
+ */
 export const userLogout = () => http.get('/user/logout');
